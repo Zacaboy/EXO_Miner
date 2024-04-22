6c3fb1fc844b704ef3e1ebf7f6b3b13184e5115a
Andrew <1026409@student.sae.edu.au>
2024-04-22 14:53:53 +0800

    Systems Merge of Levels Completed

--- Project_EXO/Assets/Scenes/Snow Assets/Rocks and Boulders 2/Shaders/RotateGameObject.cs
+++ Project_EXO/Assets/Scenes/Snow Assets/Rocks and Boulders 2/Shaders/RotateGameObject.cs
@@ -8,15 +8,14 @@ public class RotateGameObject : MonoBehaviour {
 	public bool local=false;
 	
 	// Use this for initialization
-	void Start () 
-	{ 
+	void Start () { 
 	
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		if (local) {
-            transform.RotateAroundLocal(transform.up, Time.fixedDeltaTime * rot_speed_x);
+			transform.RotateAroundLocal(transform.up, Time.fixedDeltaTime*rot_speed_x);
 		} else {
 			transform.Rotate(Time.fixedDeltaTime*new Vector3(rot_speed_x,rot_speed_y,rot_speed_z), Space.World);
 		}
