85ce6868befc76d9e7b0bc19da2ad94300c74a7e
Nicholas Clark <xenaxv2101@gmail.com>
2024-04-24 13:39:09 +0800

    Fixed Cursor and Gamemanager
    remember to use prefab

--- Project_EXO/Assets/Scripts/UIButton.cs
+++ Project_EXO/Assets/Scripts/UIButton.cs
@@ -6,20 +6,27 @@ using TMPro;
 
 public class UIButton : MonoBehaviour
 {
+    // Variables
     public AudioClip clickSFX;
+
+    // Ignore
     TextMeshProUGUI text;
     AudioSource source;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Sets the variables
         text = GetComponentInChildren<TextMeshProUGUI>();
         source = GetComponent<AudioSource>();
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Assigns the data
         text.text = name;
     }
 
