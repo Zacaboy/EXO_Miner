7f3e95f8a709cba799ebe42345bb9bab7dfd22be
Nicholas Clark <xenaxv2101@gmail.com>
2024-04-24 17:43:07 +0800

    Added distance tracker to objective panel
    Fixed my level intro

--- Project_EXO/Assets/Scripts/ObjectiveTracker.cs
+++ Project_EXO/Assets/Scripts/ObjectiveTracker.cs
@@ -4,32 +4,13 @@ using UnityEngine;
 using TMPro;
 using Unity.VisualScripting;
 
-public enum Type { Auto, Manuel }
-
 public class ObjectiveTracker : MonoBehaviour
 {
-    public Type type;
-    public TMP_Text distanceText;
-
-    [Header("Manuel")]
-    public Transform initialTargetLocation;
-    public Transform alternateTargetLocation;
-    public GameObject[] objectsToTrack;
+    public TextMeshProUGUI distanceText;
     [HideInInspector] public Transform targetLocation;
 
     private void Start()
     {
-        if (type == Type.Manuel)
-            if (initialTargetLocation == null)
-            {
-                Debug.LogError("Initial target location is not assigned!");
-                enabled = false;
-            }
-            else
-            {
-                targetLocation = initialTargetLocation;
-            }
-
         if (distanceText == null)
         {
             Debug.LogError("Distance text object is not assigned!");
@@ -53,41 +34,11 @@ public class ObjectiveTracker : MonoBehaviour
             distanceText.text = "Explore The Environment";
         }
 
-        // This is for the Auto method
-        if (type == Type.Auto)
-            if (ObjectiveManager.me.currentObjective.type != ObjectiveType.CollectResources)
-                targetLocation = ObjectiveManager.me.currentObjective.group.transform;
-            else
-                targetLocation = null;
+        // This finds the current objective
+        if (ObjectiveManager.me.currentObjective.type != ObjectiveType.CollectResources)
+            targetLocation = ObjectiveManager.me.currentObjective.group.transform;
+        else
+            targetLocation = null;
 
-        // This is for the Manuel method
-        if (objectsToTrack.Length == 0 & type == Type.Manuel)
-        {
-            // Change the target location to the alternate one
-            if (alternateTargetLocation != null)
-            {
-                targetLocation = alternateTargetLocation;
-            }
-            else
-            {
-                Debug.LogError("Alternate target location is not assigned!");
-                enabled = false;
-            }
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        // Check if any tracked objects have been destroyed
-        if (type == Type.Manuel)
-            for (int i = 0; i < objectsToTrack.Length; i++)
-            {
-                if (objectsToTrack[i] == null)
-                {
-                    // Remove the destroyed object from the array
-                    objectsToTrack[i] = objectsToTrack[objectsToTrack.Length - 1];
-                    System.Array.Resize(ref objectsToTrack, objectsToTrack.Length - 1);
-                }
-            }
     }
 }
